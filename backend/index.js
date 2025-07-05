e419df64cf9edff3e2e9ae1f84857fd0f22ee8e0
nrain78 <noorrafique7890>
2025-07-05 15:49:26 +0500

    chnages

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://product-catalog-react-frontend.vercel.app"
+    "https://product-catalogss-frontend.vercel.app/"
   ]
 }));
 
